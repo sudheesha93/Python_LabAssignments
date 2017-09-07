4d9c1a74644aac546115973bd53271cebcdd9d61
sudheesha93 <sudheesha93@gmail.com>
2017-09-07 17:20:44 -0500

    lab1

--- Lab1/Source/Task_2.py
+++ Lab1/Source/Task_2.py
@@ -1,11 +1,14 @@
 # Program to check if a given string contain all the 26 alphabets
 import string
 
-str = input("Enter an input string: ")                # String input from the user
+stringinput= input("Enter an input string: ")                       # String input from the user
 
-alphastring = set(string.ascii_lowercase)                  # creating a default set which contains all the 26 alphabets in lowercase
+alphastring = set(string.ascii_lowercase)                           # creating a default set which contains all the 26 alphabets in lowercase
 
-if set(str.lower()) >= alphastring:                   # condition for checking whether the input string has all the alphabets
-    print ("\n The string "+str+ " contains all the 26 alphabets")
+print(" \n All the letters in the random order \n")
+print(alphastring)
+
+if set(stringinput.lower()) >= alphastring:                           # condition for checking whether the input string has all the alphabets
+    print ("\n The string "+stringinput+ " contains all the 26 alphabets")
 else:
-    print("\n The string does'nt have all the alphabets")
+    print("\n The string " +stringinput+ " does'nt have all the alphabets")
--- Lab1/Source/Task_3.py
+++ Lab1/Source/Task_3.py
@@ -2,12 +2,12 @@
 
 # Creating a list to add all the numbers which satisfy the given condition
 
-numbers = []
+numberslist = []
 
-for i in range(700, 1700):                                      # range of the numbers is 700 to 1700
-    if i%5 == 0 and i%2 == 0:                                  # Checking the condition- number divisible by 5 and also multiple of 2
-        numbers.append(i)                                 # If the number satisfies the condition append that number to the list
+for i in range(700, 1701):                                      # range of the numbers is 700 to 1700
+    if i%5 == 0 and i%2 == 0:                                   # Checking the condition- number divisible by 5 and also multiple of 2
+        numberslist.append(i)                                 # If the number satisfies the condition append that number to the list
 
 print("The numbers between 700 and 1700 that are divisible by 5 & also multiple of 2 are: \n")
-print(numbers)                                                  # Dispaly all numbers from the list
+print(numberslist)                                                  # Dispaly all numbers from the list
 
