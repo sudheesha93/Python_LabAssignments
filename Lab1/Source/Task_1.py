e5879aa26d4dc8764920f157232010c66cccba70
sudheesha93 <sudheesha93@gmail.com>
2017-09-07 18:00:34 -0500

    screenshots

--- Lab1/Source/Task_1.py
+++ Lab1/Source/Task_1.py
@@ -2,17 +2,17 @@
 # TO CALCULATE THE FREQUENCY OF EACH WORD FROM THE FILE
 # TO DISPLAY ALL WORDS WITH THEIR FREQUENCIES
 
-count= {}
+freqwordcount= {}
 
 # Opening the file in read-write mode
 
-with open('../file.txt', 'r+') as tf:                  # To read a file and assigning some name to the file
-    for line in tf:                                    # Split the file into lines
+with open('../file.txt', 'r+') as file:                  # To read a file and assigning some name to the file
+    for line in file:                                    # Split the file into lines
         for word in line.split():                            # Split the lines into words
-            if word not in count:                               # Check if the word is already present or not
-                count[word] = 1                                 # Calculate the frequency of each word
+            if word not in freqwordcount:                               # Check if the word is already present or not
+                freqwordcount[word] = 1                                 # Calculate the frequency of each word
             else:
-                count[word] = count[word]+1                                # If the word is already present increment the count by 1
-    for k, v in count.items():                   # To display allthe words in text file along with their frequency of occurences which is equal to the wordcount
-        print(k, v)
-tf.close()
+                freqwordcount[word] = freqwordcount[word]+1                                # If the word is already present increment the count by 1
+    for i, j in freqwordcount.items():                   # To display allthe words in text file along with their frequency of occurences which is equal to the wordcount
+        print(i, j)
+file.close()
